60a1ea51c2ed9188208470c04f2916f2e5e94319
Alfred UC <b6pzeusbc54tvhw5jgpyw8pwz2x6gs@gmail.com>
2016-05-06 21:09:37 +0900

    now, return stdout, stderr

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const execSync = require('child_process').execSync;
 const objectAssign = require('object-assign');
 const colors = require('colors');
+
 colors.enabled = true;
 
 const isArray = Array.isArray || function(obj) {
@@ -9,15 +10,21 @@ const isArray = Array.isArray || function(obj) {
 };
 
 const execOptions = {
-	stdio: [ process.stdin, process.stdout, process.stderr ],
+	//stdio: [ process.stdin, process.stdout, process.stderr ],
+	encoding: 'utf8',
 	cwd: path.resolve( process.cwd() )
 };
 
-function exec( cmd, options ) {
+function execUC( cmd, options ) {
 
 	options = objectAssign( {}, execOptions, options );
 	if( isArray( cmd )) {
 		cmd = cmd.join(' ');
+	}
+
+	if( options.stdio ) {
+		console.log('now, stdio option is not allow yet.');
+		delete options.stdio;
 	}
 
 	const prompt = options.cwd.cyan + '$ ';
@@ -25,9 +32,38 @@ function exec( cmd, options ) {
 	console.log('');
 	console.log('--------------------start exec------------------'.cyan);
 	console.log( prompt.magenta + cmd );
-	execSync( cmd, options );
+
+	const result = { stdout: '', stderr: '', status: 0 };
+
+	//const originalStdoutWrite = process.stdout.write;
+	const originalStderrWrite = process.stderr.write;
+
+	process.stderr.write = function() {};
+
+	try {
+		result.stdout = execSync( cmd, options );
+	} catch( err ) {
+
+		//process.stdout.write = originalStdoutWrite;
+		process.stderr.write = originalStderrWrite;
+
+		result.status = err.status;
+		result.stderr = err.stderr;
+
+		console.error( err.stderr );
+
+		console.log('--------------------/end exec with error---------'.yellow);
+		console.log('');
+		return result;
+	}
+	//process.stdout.write = originalStdoutWrite;
+	process.stderr.write = originalStderrWrite;
+
+	console.log( result.stdout );
+
 	console.log('--------------------/end exec-------------------'.cyan);
 	console.log('');
+	return result;
 }
 
-module.exports = exec;
+module.exports = execUC;
--- test.js
+++ test.js
@@ -1,20 +1,13 @@
 const execSync = require('./index.js');
-const fs = require('fs');
 
 // test1
-execSync(['ls','-a']);
+execSync(['ls','-al']);
 
 // test2
-const options = {
-	stdio: [ process.stdin, process.stdout, fs.openSync('err.out', 'w') ]
-};
-
 execSync(['rm', '-rf', 'test_dir' ]);
 execSync(['mkdir', 'test_dir' ]);
-try {
-	execSync(['mkdir', 'test_dir'], options );
-} catch( err ) {
-
-	console.log('An error occurred in execSync. check err.out file');
+const result = execSync(['mkdir', 'test_dir']);
+if( result.status !== 0 ) {
+	console.log('An error occurred in execSync. Now, check err.out file');
 	execSync(['cat','err.out']);
 }
